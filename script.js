6bcbc9cbd25b7f3a4bf33f7be8f3da990ace5b2e
Matheus <joaoomatheus@hotmail.com>
2020-08-26 11:48:31 -0300

    Adicionada a função finalizar()

--- script.js
+++ script.js
@@ -20,7 +20,39 @@ function inLista(n, l) {
 function adicionar() {
     if (isNumero(num.value) && inLista(num.value, valores)) {
         valores.push(Number(num.value))
+        let opt = document.createElement('option')
+        opt.innerHTML = `Número ${num.value} adicionado.`
+        lista.appendChild(opt)
+        
     } else {
-        window.alert("Número inválido")
+        window.alert("Número inválido ou já existente na lista.")
     }
+    num.value = ''
+    num.focus()
+    result.innerHTML = ''
 };
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar')
+        return
+    } else {
+        var maior = valores[0]
+        var menor = valores[0]
+        var media = 0
+        var soma = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            } else if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+    }
+    result.innerHTML = `<p>Temos um total de ${valores.length} números cadastrados.</p>`
+    result.innerHTML += `<p>A soma dos números registrados é ${soma}.</p>`
+    result.innerHTML += `<p>O maior número cadastrado é ${maior}.</p>`
+    result.innerHTML += `<p>O menor número cadastrado é ${menor}.</p>`
+    result.innerHTML += `<p>A média dos números castrados é ${soma/valores.length}.</p>`
+}
