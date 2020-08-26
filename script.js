cbad236d975681972243cc0ae6a573c04a5cb1a4
Matheus <joaoomatheus@hotmail.com>
2020-08-26 11:30:17 -0300

    Criada funções inLista e isNumero

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-var num = document.getElementById('#txtn');
-var lista = document.getElementById('#lista');
-var result = document.getElementById('#res');
+var num = document.getElementById('txtn');
+var lista = document.getElementById('lista');
+var result = document.getElementById('res');
 var valores = [];
 
-function adicionar {
-    if (isNumero(num.value) && !inLista(num.value, valores)) {
+function isNumero(n) {
+    if (Number(n) <= 100 && Number(n) >= 1) {
+        return true
+    } 
+    return false
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) == - 1) {
+        return true
+    } 
+    return false
+}
+
+function adicionar() {
+    if (isNumero(num.value) && inLista(num.value, valores)) {
         valores.push(Number(num.value))
-        
+    } else {
+        window.alert("Número inválido")
     }
 };
